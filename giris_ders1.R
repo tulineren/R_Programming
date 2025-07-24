2c2bcb7606887d686f895e7beb91e2899f144119
Tülin Eren <erentulin5@gmail.com>
2025-07-24 10:36:01 +0300

    Add files via upload

---
+++ giris_ders1.R
@@ -0,0 +1,36 @@
+#Matematiksel Islemler 
+
+#Toplama
+12+5
+#Cikarma
+45-32
+#Carpma
+14*4
+#Bolme
+30/4
+#Us Alma
+10**3
+10^3
+#Tam Bolme
+10%/%3
+#Kalan Bulma(Mod Alma)
+27%%5
+
+
+# Fonksiyon tanimi: iki sayiyi toplayan
+topla <- function(a, b) { #function(a, b) ifadesi, fonksiyonun iki parametre aldigini belirtir.
+  sonuc <- a + b
+  return(sonuc)
+}
+# Fonksiyonu cagirma
+topla(3, 7)  
+
+kare_al <- function(x){
+  return(x^2)
+}
+kare_al(4)
+
+
+print("Merhaba R ogreniyorum.")
+#Turkce karakter kullanmamaya dikkat edin!
+
